997efac17de34b12e9d1254866d2f62b5f133467
Zach <Lavakz98@uwosh.edu>
2023-07-26 07:01:11 -0500

    combine all states

--- data_post_creator.py
+++ data_post_creator.py
@@ -5,24 +5,32 @@ DATA_FILENAME = "income.csv"
 OUTPUT_FILENAME = "testData"
 MARKER_FILENAME = "markerData.json"
 ZIP_PROP_NAME = "ZCTA5CE10"
-ROUND_COORDS = 4
+ROUND_COORDS = 3
+
 
 def main():
     """main"""
-    state_zips_filenames = ["../State-zip-code-GeoJSON/wi_wisconsin_zip_codes_geo.min.json",
-                            "../State-zip-code-GeoJSON/il_illinois_zip_codes_geo.min.json"]
+    state_zips_filenames = [
+        "../State-zip-code-GeoJSON/wi_wisconsin_zip_codes_geo.min.json",
+        "../State-zip-code-GeoJSON/il_illinois_zip_codes_geo.min.json",
+        "../State-zip-code-GeoJSON/mn_minnesota_zip_codes_geo.min.json",
+        "../State-zip-code-GeoJSON/ia_iowa_zip_codes_geo.min.json",
+        "../State-zip-code-GeoJSON/oh_ohio_zip_codes_geo.min.json"
+    ]
 
     all_states = []
     for zip_data_filename in state_zips_filenames:
+        UI.print_name(zip_data_filename.rsplit('/', maxsplit=1)[-1])
         state_features = create_features(zip_data_filename)
-        summarize(state_features)
-        # write(state_features, zip_data_filename.split('/')[-1])
+        UI.summarize(state_features)
         all_states.append(state_features)
 
-    write(all_states[0] + all_states[1], "testData")
+    # combine each list of features and write
+    IO.write([e for l in all_states for e in l], "testData")
+
 
 def create_features(filename):
-    """creates a list of geoJSON featuress"""
+    """creates a list of geoJSON features"""
     # get input data
     global database
     database = load_database(DATA_FILENAME)
@@ -30,7 +38,7 @@ def create_features(filename):
     # get marker data
     global marker_zip_set
     marker_zip_set = set()  # set of zip codes with markers in them
-    for marker in load_json(MARKER_FILENAME)["marker_data"]:  # from markerData.js
+    for marker in IO.load_json(MARKER_FILENAME)["marker_data"]:  # from markerData.js
         marker_zip_set.add(marker["ZIP"])
 
     # remove zips that don't include markers then round remaining coordinates
@@ -41,14 +49,18 @@ def create_features(filename):
 
     return marker_zips
 
+
 def get_marker_zips(zip_data_filename) -> list:
     """remove geoJSON features with zips that are not in marker_zip_set"""
+
     def is_marker_zip(zip_data) -> bool:
-        return True if zip_data["properties"][ZIP_PROP_NAME] in marker_zip_set else False
-    return list(filter(is_marker_zip, load_json(f"{zip_data_filename}")["features"]))
+        return (
+            True if zip_data["properties"][ZIP_PROP_NAME] in marker_zip_set else False
+        )
+
+    return list(filter(is_marker_zip, IO.load_json(f"{zip_data_filename}")["features"]))
 
 
-# round coordinates
 def round_coordinates(data, precision) -> list:
     def apply(item, fun):
         if isinstance(item, list):
@@ -60,19 +72,6 @@ def round_coordinates(data, precision) -> list:
             feature["geometry"]["coordinates"], lambda x: round(x, precision)
         )
     return data
-
-
-
-def load_json(filename) -> dict:
-    """returns data loaded from a json file"""
-    with open(filename, "r", encoding="UTF-8") as file:
-        return json.load(file)
-
-
-def load_csv(filename):
-    """returns data loaded from a csv file"""
-    with open(filename, "r", encoding="UTF-8") as file:
-        return pd.read_csv(file.read())
 
 
 def load_database(filename) -> dict:
@@ -107,34 +106,54 @@ def get_data(zipcode) -> int:
         return 0
 
 
-def write(data, output_filename):
-    """create the output file"""
-    with open(f"{output_filename}.js", "w", encoding="UTF-8") as out:
-        out.write(f"testData = ")
-        out.write(json.dumps(data))
+class IO:
+    @staticmethod
+    def write(data, output_filename):
+        """create the output file"""
+        with open(f"{output_filename}.js", "w", encoding="UTF-8") as out:
+            out.write(f"testData = ")
+            out.write(json.dumps(data))
+
+    @staticmethod
+    def load_json(filename) -> dict:
+        """returns data loaded from a json file"""
+        with open(filename, "r", encoding="UTF-8") as file:
+            return json.load(file)
+
+    @staticmethod
+    def load_csv(filename):
+        """returns data loaded from a csv file"""
+        with open(filename, "r", encoding="UTF-8") as file:
+            return pd.read_csv(file.read())
 
 
-def summarize(data):
-    """summarize data"""
-    print(f"\nKEYS:\n{key_str(data)}\n")
-    print(f'DATA:\n{prop_str(data, "DATA")}\n')
-    print(f'COORDS:\n{data[0]["geometry"]["coordinates"][0][:5]}\n')
-    print(f"LENGTH:\n{len(data)}\n")
+class UI:
+    @staticmethod
+    def print_name(name):
+        print(f"\n ########## {name} ########## ")
 
+    @staticmethod
+    def summarize(data):
+        """summarize data"""
+        print(f"\nKEYS:\n{UI.key_str(data)}")
+        print(f'DATA:\n{UI.prop_str(data, "DATA")}')
+        print(f'COORDS:\n{data[0]["geometry"]["coordinates"][0][:5]}')
+        print(f"LENGTH:\n{len(data)}")
 
-def key_str(data: dict) -> str:
-    """returns a string of the dictonary keys"""
-    return ", ".join(data[0]["properties"].keys())
+    @staticmethod
+    def key_str(data: dict) -> str:
+        """returns a string of the dictonary keys"""
+        return ", ".join(data[0]["properties"].keys())
 
-
-def prop_str(data: dict, prop: str, count: int = 10) -> str:
-    """returns a string of the given property"""
-    new_prop_str = ""
-    for i, feature in enumerate(data):
-        new_prop_str += f'{feature["properties"][prop]}, '
-        if i == count:
-            break
-    return new_prop_str[: len(new_prop_str) - 2]
+    @staticmethod
+    def prop_str(data: dict, prop: str, count: int = 10) -> str:
+        """returns a string of the given property"""
+        new_prop_str = ""
+        for i, feature in enumerate(data):
+            new_prop_str += f'{feature["properties"][prop]}, '
+            if i == count:
+                break
+        return new_prop_str[: len(new_prop_str) - 2]
 
 
 if __name__ == "__main__":
